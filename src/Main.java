9ab3c395a760be784f81acb81b2722f68f5b4bce
joje3029 <joje3029@gmail>
2023-09-15 17:16:55 +0900

    원인과 목적을 분명히 해야 생각의 길이 방향을 안잃는다-우영쌤. 원인은 알았는데 함수로 빼는 목적을 생각을 안해서 함수를 두는 위치를 못찾았고 그래서 생각이 산으로 감

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 	static List<Article> articles = new ArrayList<Article>();
-
+	static Article article;
 	public static void main(String[] args) {
 
 		System.out.println("== 프로그램 시작 ==");
-		
+
 		makeTestData();
-//		이 메소드가 호출 되면 임시 데이터가 만들어 져야함. 
-//		쌤이 준 힌트 키워드 : 지역변수, 인스턴스 변수 -> articles에 접근하기 위해 써야하는 메서드?
-//		생성자와 메서드 오버로딩
-		
+		fordata(); // 순회하면서 게시글 찾는 부분 중복 제거
+//		for( get()) : list, detail, delete, modify
 
 		Scanner sc = new Scanner(System.in);
-		int lastArticleId = 0;
-
+		int lastArticleId = 3;
 
 		while (true) {
 
@@ -37,11 +34,13 @@ public class Main {
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다");
 				} else {
-					System.out.println("번호     /    제목     /    조회   ");
-					for (int i = articles.size() - 1; i >= 0; i--) {
-						Article article = articles.get(i);
-						System.out.printf(" %4d     /   %5s    /      %4d  \n", article.id, article.title, article.hit);
-					}
+					System.out.println("번호      /    제목     /    조회   ");
+					fordata();
+//					for (int i = articles.size() - 1; i >= 0; i--) {
+//						Article article = articles.get(i);
+//						System.out.printf(" %4d     /   %5s    /      %4d  \n", article.id, article.title, article.hit);
+//					}
+					System.out.printf(" %4d     /   %5s    /      %4d  \n", article.id, article.title, article.hit);
 				}
 
 			} else if (command.equals("article write")) {
@@ -52,32 +51,33 @@ public class Main {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
-				Article article = new Article(id, regDate, regDate, title, body);
+				article = new Article(id, regDate, regDate, title, body);
 				articles.add(article);
 
 				System.out.printf("%d번글이 생성되었습니다.\n", id);
 				lastArticleId++;
 			} else if (command.startsWith("article detail")) {
 
-				String[] commandDiv = command.split(" "); // article detail 1
+				String[] commandDiv = command.split(" "); 
 
 				int id = Integer.parseInt(commandDiv[2]);
 
 				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
+				
+				fordata();
+//				for (int i = 0; i < articles.size(); i++) {
+//					Article article = articles.get(i);
 					if (article.id == id) {
 						foundArticle = article;
-						break;
-					}
+//						break;
+//					}
 				}
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 없어\n", id);
 					continue;
 				}
-				
+
 				foundArticle.hit++;
 
 				System.out.println("번호 : " + foundArticle.id);
@@ -95,12 +95,13 @@ public class Main {
 
 				Article foundArticle = null;
 
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
+				fordata();
+//				for (int i = 0; i < articles.size(); i++) {
+//					Article article = articles.get(i);
 					if (article.id == id) {
 						foundArticle = article;
-						break;
-					}
+//						break;
+//					}
 				}
 
 				if (foundArticle == null) {
@@ -126,19 +127,21 @@ public class Main {
 
 				int foundIndex = -1;
 
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
+				fordata();
+//				for (int i = 0; i < articles.size(); i++) {
+//					Article article = articles.get(i);
 					if (article.id == id) {
-						foundIndex = i;
-						break;
-					}
+						foundIndex = id; //테스트후 수정(원래 i)
+//						break;
+//					}
 				}
 
 				if (foundIndex == -1) {
 					System.out.printf("%d번 게시물은 없어\n", id);
 					continue;
 				}
-
+				
+				
 				articles.remove(foundIndex);
 				System.out.println(id + "번 글을 삭제했어");
 
@@ -153,23 +156,18 @@ public class Main {
 		sc.close();
 	}
 
-	private static void makeTestData() {
-//		add할 테스트 데이터
-		
-//		반복으로 생겼으면 좋겠어 이건 노가다잖아
-//		반복 = 반복문 -> for문을 돌린다!
-		for(int i=1; i<=3; i++) {
-			int id = i;
-			String regDate = Util.getNow();
-			String title = "용감한 뱁새"+i;
-			String body = "레벨"+i+"업!!";
-			Article article = new Article(id, regDate, regDate, title, body);
-			articles.add(article);
-			
+	private static void fordata() {
+//		System.out.println("fordata 동작함!");
+		for (int i = 0; i < articles.size(); i++) {
+			article = articles.get(i);
 		}
-		
-		System.out.println("testdata 3개 생성됨");
-		
+	}
+
+	private static void makeTestData() {
+		System.out.println("테스트를 위한 데이터 3개 생성 완료");
+		articles.add(new Article(1, Util.getNow(), Util.getNow(), "제목1", "내용1", 11));
+		articles.add(new Article(2, Util.getNow(), Util.getNow(), "제목2", "내용2", 22));
+		articles.add(new Article(3, Util.getNow(), Util.getNow(), "제목3", "내용3", 33));
 	}
 }
 
@@ -182,14 +180,15 @@ class Article {
 	int hit;
 
 	Article(int id, String regDate, String updateDate, String title, String body) {
+		this(id, regDate, updateDate, title, body, 0);
+	}
+
+	Article(int id, String regDate, String updateDate, String title, String body, int hit) {
 		this.id = id;
 		this.regDate = regDate;
 		this.updateDate = updateDate;
 		this.title = title;
 		this.body = body;
-		this.hit = 0;
+		this.hit = hit;
 	}
-		
 }
-
-
