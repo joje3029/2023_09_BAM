f91469c9dd9b24f555d09678e835269fe83c5004
joje3029 <joje3029@gmail>
2023-09-15 15:40:51 +0900

    테스트 데이터 생성까지 함! 용감한 뱁새!

--- src/Main.java
+++ src/Main.java
@@ -1,118 +1,104 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-   public static void main(String[] args) {
+	static List<Article> articles = new ArrayList<Article>();
 
-      System.out.println("== 프로그램 시작 ==");
+	public static void main(String[] args) {
 
-      Scanner sc = new Scanner(System.in);
-      int lastArticleId = 0; //글 번호
-      
+		System.out.println("== 프로그램 시작 ==");
+		
+		makeTestData();
+//		이 메소드가 호출 되면 임시 데이터가 만들어 져야함. 
+//		쌤이 준 힌트 키워드 : 지역변수, 인스턴스 변수 -> articles에 접근하기 위해 써야하는 메서드?
+//		생성자와 메서드 오버로딩
+		
 
-      ArrayList<Article> articles = new ArrayList<>();
-//      ArrayList<String> articleListTitle = new ArrayList<>();
-//      ArrayList<String> articleListbody = new ArrayList<>();
-//      근데 굳이 array list를 두개 쓰고 싶지 않음. -> 객체화를 안해서 그럼 그래서 두개가 한번에 append되지 않은거고.
-      
-      while (true) {
-         
-         System.out.printf("명령어 ) ");
-         String command = sc.nextLine();
+		Scanner sc = new Scanner(System.in);
+		int lastArticleId = 0;
 
-         if (command.length() == 0) {
-            System.out.println("너 명령어 입력 안했어");
-            continue;
-         }
 
-         if (command.equals("exit")) {
-            break;
-         }
+		while (true) {
 
-         if (command.equals("article list")) {
-        		if (articles.size() == 0) {
+			System.out.printf("명령어 ) ");
+			String command = sc.nextLine().trim();
+
+			if (command.length() == 0) {
+				System.out.println("너 명령어 입력 안했어");
+				continue;
+			}
+
+			if (command.equals("exit")) {
+				break;
+			}
+
+			if (command.equals("article list")) {
+				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다");
 				} else {
-					System.out.println("번호   /    제목   /   조회 ");
+					System.out.println("번호     /    제목     /    조회   ");
 					for (int i = articles.size() - 1; i >= 0; i--) {
 						Article article = articles.get(i);
-						System.out.printf(" %d     /   %s  /  %d\n", article.id, article.title, article.view);
+						System.out.printf(" %4d     /   %5s    /      %4d  \n", article.id, article.title, article.hit);
 					}
 				}
 
-         } else if (command.equals("article write")) {
-            int id = lastArticleId + 1;
-            int view = 0;
-            System.out.printf("제목 : ");
-            String title = sc.nextLine();
-            System.out.printf("내용 : ");
-            String body = sc.nextLine();
-         
-         // 현재 날짜 구하기
-            LocalDate now = LocalDate.now();
-     
-            // 포맷 정의
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-     
-            // 포맷 적용
-            String formatedNow = now.format(formatter);
-            
-            Article article = new Article(id, title, body, formatedNow, view);
-            
-            articles.add(article);
-            
+			} else if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				String regDate = Util.getNow();
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
 
-            System.out.printf("%d번글이 생성되었습니다.\n", id);
-            lastArticleId++;
-            
-            
-         } else if(command.startsWith("article detail")){ //detail일 때
-        	 //2.조회수 기능은 클릭되었을때 즉 detail을 입력하면 증가하고 증가한게 보여야 함.
-        	 //조회수가 보이게도 해야하고 클릭했을때 그 변수가 증가해야겠네. 0으로 생성은 글 쓰고 저장할때 되야겠다.
-        	 //그럼 같이 객체로 넣는게 낫겠다.
-        	 //조회수 변수명 : views
-        	 
-        	 String[] str= command.split(" "); 
-             String num =str[2];
-             int id =Integer.parseInt(num);
-             
-             boolean found = false;
-             
-            for(int i=0; i<articles.size(); i++) {
-            	Article article = articles.get(i);
-            		if(article.id == id) {
-            			found = true;
-            			article.view++;
-                        System.out.printf("번호 : %d\n", article.id );
-                        System.out.printf("날짜 : %s\n", article.formatedNow); 
-                        System.out.printf("제목 : %s\n", article.title);
-                        System.out.printf("내용 : %s\n", article.body);
-                        System.out.printf("조회수 : %d\n", article.view);
-                        break;
+				Article article = new Article(id, regDate, regDate, title, body);
+				articles.add(article);
 
-            		}
-            	}
-           
-            if (found == false) {
-				System.out.printf("%d번 게시물은 없어\n", id);
-			}
-            //우영쌤이 과제로 낸 코드
-         } else if (command.startsWith("article delete")) {
+				System.out.printf("%d번글이 생성되었습니다.\n", id);
+				lastArticleId++;
+			} else if (command.startsWith("article detail")) {
 
-				String[] commandDiv = command.split(" "); // article delete 1
+				String[] commandDiv = command.split(" "); // article detail 1
 
-				int id = Integer.parseInt(commandDiv[2]); // 숫자 끊어서 id에 저장
+				int id = Integer.parseInt(commandDiv[2]);
 
-				boolean found = false; // boolean타입 found를 f로 지정
-				Article foundArticle = null; // article 타입의 foundArticle 을 null로 선언
-				int i =0;
-				for (i = 0; i < articles.size(); i++) { // articles를 순회함 
-					Article article = articles.get(i);	// 하나씩 꺼
-					if (article.id == id) { // 하나씩 꺼낸 article의 id와 위에서 id로 저장한 숫자가 같으면
-						found = true; //found가 true로
-						foundArticle = article; //foundAticle의 값이 id가 같은 article로 변경
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 없어\n", id);
+					continue;
+				}
+				
+				foundArticle.hit++;
+
+				System.out.println("번호 : " + foundArticle.id);
+				System.out.println("작성날짜 : " + foundArticle.regDate);
+				System.out.println("수정날짜 : " + foundArticle.updateDate);
+				System.out.println("제목 : " + foundArticle.title);
+				System.out.println("내용 : " + foundArticle.body);
+				System.out.println("조회수 : " + foundArticle.hit);
+
+			} else if (command.startsWith("article modify")) {
+
+				String[] commandDiv = command.split(" ");
+
+				int id = Integer.parseInt(commandDiv[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
 						break;
 					}
 				}
@@ -122,70 +108,88 @@ public class Main {
 					continue;
 				}
 
-				articles.remove(i); //여기를 고침 기존 id에서 순회했을때 인덱스를 기준으로 삭제하게
-				
+				System.out.printf("제목 : ");
+				String newTitle = sc.nextLine();
+				System.out.printf("내용 : ");
+				String newBody = sc.nextLine();
+
+				String updateDate = Util.getNow();
+				foundArticle.title = newTitle;
+				foundArticle.body = newBody;
+				foundArticle.updateDate = updateDate;
+
+			} else if (command.startsWith("article delete")) {
+
+				String[] commandDiv = command.split(" "); // article delete 1
+
+				int id = Integer.parseInt(commandDiv[2]);
+
+				int foundIndex = -1;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundIndex = i;
+						break;
+					}
+				}
+
+				if (foundIndex == -1) {
+					System.out.printf("%d번 게시물은 없어\n", id);
+					continue;
+				}
+
+				articles.remove(foundIndex);
 				System.out.println(id + "번 글을 삭제했어");
 
-           
-         }else if(command.startsWith("article modify")){// 1. article modify로 명령문 : 게시글 수정
-//        	 게시글 수정은 몇번 글인지 선택을 해야하니 detail/ delete랑 비슷
-        	 String[] str= command.split(" "); 
-             String num =str[2];
-             int id =Integer.parseInt(num);
-             //여기까지 modify 뒤의 번호를 id라는 변수에 저장한것
-             boolean found = false; //일단 못찾았을때
-             
-             for(int i=0; i<articles.size(); i++) { // 순회
-             	Article article = articles.get(i); 
-             		if(article.id == id) { // 찾으려는 게시물 있는 경우
-             			found = true; //있으니까 true로 변경
-             			//새로 받을 제목이랑 내용
-             			System.out.printf("제목 : "); 
-                        String title = sc.nextLine();
-                        System.out.printf("내용 : ");
-                        String body = sc.nextLine();
-                     
-                        //여기서 부터 수정되야겠네
-                        //아이디가 맞는 걸 찾았으니 그 인덱스에 들어있는 title이랑 body에 담긴걸 새로 입력한 title과 body로 변경하면 되는거잖아.
-                        article.title = title;
-                        article.body = body;
-                        
-                        System.out.printf("%d번글이 수정되었습니다.\n", id);
-                        
-                         break;
+			} else {
+				System.out.println("존재하지 않는 명령어입니다");
+				continue;
+			}
+		}
 
-             		}
-             		
-        		}
-             if (found == false) {
-            	 System.out.printf("%d번 게시물은 없어\n", id);
-             }
-        	 
-         }else {
-            System.out.println("존재하지 않는 명령어입니다");
-            continue;
-         }
-      }
+		System.out.println("== 프로그램 종료 ==");
 
-      System.out.println("== 프로그램 종료 ==");
+		sc.close();
+	}
 
-      sc.close();
-   }
-   
+	private static void makeTestData() {
+//		add할 테스트 데이터
+		
+//		반복으로 생겼으면 좋겠어 이건 노가다잖아
+//		반복 = 반복문 -> for문을 돌린다!
+		for(int i=1; i<=3; i++) {
+			int id = i;
+			String regDate = Util.getNow();
+			String title = "용감한 뱁새"+i;
+			String body = "레벨"+i+"업!!";
+			Article article = new Article(id, regDate, regDate, title, body);
+			articles.add(article);
+			
+		}
+		
+		System.out.println("testdata 3개 생성됨");
+		
+	}
 }
 
-class Article{
-	int view;
-	String formatedNow;
-    int id;
-    String title;
-    String body;
-   
-   Article(int id, String title, String body, String formatedNow, int view){
-	  this.view = view;
-      this.id = id;
-      this.title = title;
-      this.body = body;
-      this.formatedNow = formatedNow;
-   }   
+class Article {
+	int id;
+	String regDate;
+	String updateDate;
+	String title;
+	String body;
+	int hit;
+
+	Article(int id, String regDate, String updateDate, String title, String body) {
+		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
+		this.title = title;
+		this.body = body;
+		this.hit = 0;
+	}
+		
 }
+
+
