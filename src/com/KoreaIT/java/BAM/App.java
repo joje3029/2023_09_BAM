14cc9acb798d25391583d725cc924a15dad2bbe5
joje3029 <joje3029@gmail.com>
2023-09-20 14:26:20 +0900

    controller 분리함

---
+++ src/com/KoreaIT/java/BAM/App.java
@@ -0,0 +1,72 @@
+package com.KoreaIT.java.BAM;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.KoreaIT.java.BAM.dto.Article;
+import com.KoreaIT.java.BAM.dto.Member;
+
+import controller.ArticleController;
+import controller.MemberController;
+
+public class App {
+
+	List<Article> articles;
+	List<Member> members;
+
+	App() {
+		articles = new ArrayList<Article>();
+		members = new ArrayList<Member>();
+	}
+
+	public void start() {
+		System.out.println("== 프로그램 시작 ==");
+
+
+		Scanner sc = new Scanner(System.in);
+
+		MemberController memberController = new MemberController(members,sc);
+		ArticleController articleController = new ArticleController(articles,sc);
+		
+		articleController.makeTestData();
+
+		while (true) {
+
+			System.out.printf("명령어 ) ");
+			String command = sc.nextLine().trim();
+
+			if (command.length() == 0) {
+				System.out.println("너 명령어 입력 안했어");
+				continue;
+			}
+			if (command.equals("exit")) {
+				break;
+			}
+			if (command.equals("member join")) {
+				memberController.doJoin();
+			}
+			else if (command.startsWith("article list")) {
+				articleController.dolist(command);
+			} else if (command.equals("article write")) {
+				articleController.dowrite();
+			} else if (command.startsWith("article detail")) {
+				articleController.detail(command);
+			} else if (command.startsWith("article modify")) {
+				articleController.modify(command);
+			} else if (command.startsWith("article delete")) {
+				articleController.delete(command);
+			} else {
+				System.out.println("존재하지 않는 명령어입니다");
+				continue;
+			}
+		}
+
+		System.out.println("== 프로그램 종료 ==");
+
+		sc.close();
+
+	}
+	
+	
+}
